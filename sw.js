98d716c5c43b95869e0099efc2597d991a915072
stefanozarfati <stefanozarfati@gmail.com>
2026-06-30 13:53:12 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oservice-suite-v3';
+const CACHE='oservice-suite-v4';
 const ASSETS=[
   './','./index.html','./attestati.html','./impaginatore.html','./manifest.json',
   './lib/pdf-lib-bundle.js',
