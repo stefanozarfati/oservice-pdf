6fa8fa02bd897c7e86c44c50c0f62a1fa17dd29c
stefanozarfati <stefanozarfati@gmail.com>
2026-09-02 18:42:20 +0200

    Bump cache v24 -> v25 (invalidazione dopo layout responsive Classifiche)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oservice-suite-v24';
+const CACHE='oservice-suite-v25';
 const ASSETS=[
   './','./index.html','./attestati.html','./impaginatore.html','./comprimi.html','./classifiche.html','./manifest.json',
   './gs/gs.mjs','./gs/gs.js','./gs/browser.js',  // gs.wasm (16MB) NON in precache: cache-first al primo uso
