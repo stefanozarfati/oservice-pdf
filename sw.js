74b35b6fe11a986e069435c828f686ab308e5dbc
stefanozarfati <stefanozarfati@gmail.com>
2026-09-10 13:44:57 +0200

    Bump cache v25 -> v26 (fix Comprimi PDF)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='oservice-suite-v25';
+const CACHE='oservice-suite-v26';
 const ASSETS=[
   './','./index.html','./attestati.html','./impaginatore.html','./comprimi.html','./classifiche.html','./manifest.json',
   './gs/gs.mjs','./gs/gs.js','./gs/browser.js',  // gs.wasm (16MB) NON in precache: cache-first al primo uso
